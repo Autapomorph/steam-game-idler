fc255d3945f5150146965527f08e93cea784c072
Autapomorph <Autapomorph@users.noreply.github.com>
2026-02-20 21:09:41 +0300

    refactor(i18n): update i18n config

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -3,27 +3,31 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import translationENUS from '@/i18n/locales/en-US/translation.json';
-import translationRU from '@/i18n/locales/ru/translation.json';
+import translationRURU from '@/i18n/locales/ru-RU/translation.json';
+
+export const ns = ['translation'] as const;
+export const defaultNS = 'translation' as const;
 
 export const resources = {
   'en-US': { translation: translationENUS },
-  ru: { translation: translationRU },
+  'ru-RU': { translation: translationRURU },
 };
-
-export const ns = ['translation'] as const;
-
-export const defaultNS = 'translation' as const;
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
-    fallbackLng: 'en-US',
-    debug: process.env.NODE_ENV === 'development',
-
     ns,
     defaultNS,
+    resources,
+
+    fallbackLng: {
+      default: ['en-US'],
+      en: ['en-US'],
+      ru: ['ru-RU'],
+    },
+
+    debug: process.env.NODE_ENV === 'development',
     partialBundledLanguages: true,
 
     detection: {
